8ba97c0fc4e71761595e267e0da1206c8466ddf3
hocinebendou <hocinebendou@gmail.com>
2018-01-31 11:15:08 +0200

    augmented frequency R script

---
+++ src/main/resources/static/scripts/ciproportion.R
@@ -0,0 +1,22 @@
+# Compute confidence interval
+
+ciproportion <- function(haplocount, totalcount, observed=TRUE) {
+
+    matchperobserved <-  totalcount %/% haplocount
+    if(observed == FALSE && haplocount == 0) {
+        matchperobserved <- (totalcount + 1) %/% (haplocount + 1)
+    }
+    pbar <- 1 / matchperobserved
+    se <- sqrt(pbar * (1 - pbar)/totalcount)
+    me <- qnorm(.975) * se
+    ci <- 1 / (pbar + c(-me, me))
+    if (ci[[1]] < 0 || ci[[1]] > totalcount) {
+        if (haplocount == 0) {
+            ci[[1]] <- totalcount + 1
+        } else {
+            ci[[1]] <- totalcount
+        }
+    }
+
+    return(ci)
+}
